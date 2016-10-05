388b224205eeebd459b419510e3d7feea186ee56
Simon Mika <simon@mika.se>
2016-10-06 00:02:57 +0200

    Added facebook and google plus.

--- static/js/share-blockquotes.js
+++ static/js/share-blockquotes.js
@@ -1,13 +1,16 @@
 var quotes = document.getElementsByTagName("blockquote")
 for (var i = 0; i < quotes.length; i++){
-	var twitter = document.createElement("a")
-	twitter.href = "https://twitter.com/intent/tweet?source=webclient&text=" + encodeURIComponent(quotes[i].textContent.trim() + " via " + document.URL)
-	twitter.target = "_blank"
-	twitter.className = "share twitter"
-	quotes[i].insertBefore(twitter, quotes[i].firstChild)
-	var linkedin = document.createElement("a")
-	linkedin.href = "https://www.linkedin.com/shareArticle?mini=true&url=" + encodeURIComponent(document.URL) + "&title=" + encodeURIComponent(document.title) + "&summary=" + encodeURIComponent(quotes[i].textContent.trim()) + "&source=Simon says"
-	linkedin.target = "_blank"
-	linkedin.className = "share linkedin"
-	quotes[i].insertBefore(linkedin, quotes[i].firstChild)
+	var destinations = {
+//		googleplus: "https://plus.google.com/share?url=" + encodeURIComponent(document.URL),
+		facebook: "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(document.URL),
+		linkedin: "https://www.linkedin.com/shareArticle?mini=true&url=" + encodeURIComponent(document.URL) + "&title=" + encodeURIComponent(document.title) + "&summary=" + encodeURIComponent(quotes[i].textContent.trim()) + "&source=Simon says",
+		twitter: "https://twitter.com/intent/tweet?source=webclient&text=" + encodeURIComponent(quotes[i].textContent.trim() + " via " + document.URL)
+	}
+	for (destination in destinations) {
+		var link = document.createElement("a")
+		link.href = destinations[destination]
+		link.target = "_blank"
+		link.className = "share " + destination
+		quotes[i].insertBefore(link, quotes[i].firstChild)
+	}
 }
