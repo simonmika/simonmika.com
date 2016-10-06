dab26e83be8a100c24a904b2553034ae596dab26
Simon Mika <simon@mika.se>
2016-10-06 14:56:47 +0200

    Improved Twitter text.

--- static/js/share-blockquotes.js
+++ static/js/share-blockquotes.js
@@ -4,7 +4,7 @@ for (var i = 0; i < quotes.length; i++){
 //		googleplus: "https://plus.google.com/share?url=" + encodeURIComponent(document.URL),
 		facebook: "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(document.URL),
 		linkedin: "https://www.linkedin.com/shareArticle?mini=true&url=" + encodeURIComponent(document.URL) + "&title=" + encodeURIComponent(quotes[i].textContent.trim()) + "&summary=" + encodeURIComponent(document.title) + "&source=Simon says",
-		twitter: "https://twitter.com/intent/tweet?source=webclient&text=" + encodeURIComponent(quotes[i].textContent.trim() + " (" + document.URL + ")")
+		twitter: "https://twitter.com/intent/tweet?source=webclient&text=" + encodeURIComponent(quotes[i].textContent.trim() + " " + document.URL + " via @simonjmika")
 	}
 	for (destination in destinations) {
 		var link = document.createElement("a")
