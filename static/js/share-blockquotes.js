ec02bec1be1d38da7d8b7deace765001d122a06c
Simon Mika <simon@mika.se>
2016-12-21 12:31:23 +0100

    Added mailto sharing link.

--- static/js/share-blockquotes.js
+++ static/js/share-blockquotes.js
@@ -1,6 +1,7 @@
 var quotes = document.getElementsByTagName("blockquote")
 for (var i = 0; i < quotes.length; i++){
 	var destinations = {
+		email: "mailto:?subject=" + encodeURIComponent(quotes[i].textContent.trim()) + "&body=" + encodeURIComponent(document.URL),
 //		googleplus: "https://plus.google.com/share?url=" + encodeURIComponent(document.URL),
 		facebook: "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(document.URL),
 		linkedin: "https://www.linkedin.com/shareArticle?mini=true&url=" + encodeURIComponent(document.URL) + "&title=" + encodeURIComponent(quotes[i].textContent.trim()) + "&summary=" + encodeURIComponent(document.title) + "&source=Simon says",
